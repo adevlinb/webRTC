fc7906b5dd629704f6156e13de3adbb4315b7b93
adevlinb <adevlin.booth@gmail.com>
2023-09-11 18:24:22 -0700

    updates to final code

--- js/main.js
+++ js/main.js
@@ -1,95 +1,127 @@
+const APP_ID = "9f6a6bdefeec43f999c89e422d2c7d93";
 let peerConnection;
 let localStream;
 let remoteStream;
 
-const userOneVid = document.getElementById("user-1");
-const userTwoVid = document.getElementById("user-2");
-const offerSDP = document.getElementById("offer-sdp");
-const answerSDP = document.getElementById("answer-sdp");
-const createOfferBTN = document.getElementById("create-offer").addEventListener("click", createOffer);
-const createAnswerBTN = document.getElementById("create-answer").addEventListener("click", createAnswer);
-const addAnswerBTN = document.getElementById("add-answer").addEventListener("click", addAnswer);
+let uid = String(Math.floor(Math.random() * 10000))
+let token = null;
+let client;
 
-let googleSTUN = {
-    iceServers: [
-        { urls: ["stun:stun1.1.google.com:19302", "stun:stun2.1.google.com:19302"] }
+let servers = {
+    iceServers:[
+        {
+            urls:['stun:stun1.1.google.com:19302', 'stun:stun2.1.google.com:19302']
+        }
     ]
 }
 
-init();
-async function init() {
-    console.log("init")
-    localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
-    userOneVid.srcObject = localStream;
 
+let init = async () => {
+    client = await AgoraRTM.createInstance(APP_ID)
+    await client.login({uid, token})
+    
+    const channel = client.createChannel('main')
+    channel.join()
+
+    channel.on('MemberJoined', handlePeerJoined)
+    client.on('MessageFromPeer', handleMessageFromPeer)
+
+   localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+   document.getElementById('user-1').srcObject = localStream
 }
 
-async function createOffer() {
-    peerConnection = new RTCPeerConnection(googleSTUN);
+let handlePeerJoined = async (MemberId) => {
+    console.log('A new peer has joined this room:', MemberId)
+    createOffer(MemberId)
+}
 
-    remoteStream = new MediaStream();
-    userTwoVid.srcObject = remoteStream;
+let handleMessageFromPeer = async (message, MemberId) => {
+    message = JSON.parse(message.text)
+    console.log('Message:', message.type)
 
-    localStream.getTracks().forEach(track => {
-        peerConnection.addTrack(track, localStream);
+    if(message.type === 'offer'){
+        if(!localStream){
+            localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+            document.getElementById('user-1').srcObject = localStream
+        }
+
+        document.getElementById('offer-sdp').value = JSON.stringify(message.offer)
+        createAnswer(MemberId)
+    }
+
+    if(message.type === 'answer'){
+        document.getElementById('answer-sdp').value = JSON.stringify(message.answer)
+        addAnswer()
+    }
+
+    if(message.type === 'candidate'){
+        if(peerConnection){
+            peerConnection.addIceCandidate(message.candidate)
+        }
+    }
+}
+
+let createPeerConnection = async (sdpType, MemberId) => {
+    peerConnection = new RTCPeerConnection(servers)
+
+    remoteStream = new MediaStream()
+    document.getElementById('user-2').srcObject = remoteStream
+
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream)
     })
 
-    peerConnection.ontrack = async function(event) {
-        event.streams[0].getTracks().forEach(track => {
-            remoteStream.addTrack(track);
+    peerConnection.ontrack = async (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack(track)
         })
     }
 
-    peerConnection.onicecandidate = async function(event) {
-        if (event.candidate) offerSDP.value = JSON.stringify(peerConnection.localDescription);
+    peerConnection.onicecandidate = async (event) => {
+        if(event.candidate){
+            document.getElementById(sdpType).value = JSON.stringify(peerConnection.localDescription)
+            client.sendMessageToPeer({text:JSON.stringify({'type':'candidate', 'candidate':event.candidate})}, MemberId)
+        }
     }
+}
 
-    let offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
+let createOffer = async (MemberId) => {
+    
+    createPeerConnection('offer-sdp', MemberId)
 
-    offerSDP.value = JSON.stringify(offer)
+    let offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
 
+    document.getElementById('offer-sdp').value = JSON.stringify(offer)
+    client.sendMessageToPeer({text:JSON.stringify({'type':'offer', 'offer':offer})}, MemberId)
+}
+
+let createAnswer = async (MemberId) => {
+    createPeerConnection('answer-sdp', MemberId)
+
+    let offer = document.getElementById('offer-sdp').value
+    if(!offer) return alert('Retrieve offer from peer first...')
+
+    offer = JSON.parse(offer)
+    await peerConnection.setRemoteDescription(offer)
+    
+    let answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+
+    document.getElementById('answer-sdp').value  = JSON.stringify(answer)
+    client.sendMessageToPeer({text:JSON.stringify({'type':'answer', 'answer':answer})}, MemberId)
+}
+
+let addAnswer = async () => {
+    let answer = document.getElementById('answer-sdp').value
+    if(!answer) return alert('Retrieve answer from peer first...')
+
+    answer = JSON.parse(answer)
+
+    if(!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer)
+    }
 
 }
 
-async function createAnswer() {
-    peerConnection = new RTCPeerConnection(googleSTUN);
-
-    remoteStream = new MediaStream();
-    userTwoVid.srcObject = remoteStream;
-
-    localStream.getTracks().forEach(track => {
-        peerConnection.addTrack(track, localStream);
-    })
-
-    peerConnection.ontrack = async function(event) {
-        event.streams[0].getTracks().forEach(track => {
-            remoteStream.addTrack(track);
-        })
-    }
-
-    peerConnection.onicecandidate = async function(event) {
-        if (event.candidate) answerSDP.value = JSON.stringify(peerConnection.localDescription);
-    }
-
-    let offer = offerSDP.value;
-    if (!offer) return alert("Retrieve offer from peer first!");
-
-    offer = JSON.parse(offer);
-    await peerConnection.setRemoteDescription(offer);
-
-    let answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer);
-
-    answerSDP.value = JSON.stringify(answer);
-
-}
-
-async function addAnswer() {
-    let answer = answerSDP.value
-    if (!answer) return alert("Retrieve Answer from peer");
-    answer = JSON.parse(answer);
-    if (!peerConnection.currentRemoteDescription) {
-        peerConnection.setRemoteDescription(answer);
-    }
-} 
+init()
